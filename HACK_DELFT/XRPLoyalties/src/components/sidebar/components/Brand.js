3837919a4215712dd03ee2abe29ae036f8f5fd53
Stoyan <onlyforplaying04@gmail.com>
2023-08-30 14:12:04 +0300

    added frontend

---
+++ HACK_DELFT/XRPLoyalties/src/components/sidebar/components/Brand.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// Chakra imports
+import { Flex, useColorModeValue } from "@chakra-ui/react";
+
+// Custom components
+import { HorizonLogo } from "components/icons/Icons";
+import { HSeparator } from "components/separator/Separator";
+import Logo from "./XRPLoyalties Logo - Transparent.png";
+
+
+export function SidebarBrand() {
+  //   Chakra color mode
+  let logoColor = useColorModeValue("navy.700", "white");
+
+
+  return (
+    <Flex align='center' direction='column'>
+        <img src={Logo} style={{height: '110px', width: '100px'}}/>
+    </Flex>
+  );
+}
+
+export default SidebarBrand;
